1725938bee9769f6e993f4ff50a525d10c3cc146
Mark Allen Evans <president@worldvaporexpo.com>
2018-09-12 00:56:29 +0000

    stage

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:chris-lea/redis-server
+sudo apt-get update
+sudo apt-get install redis-server
+sudo apt-get install libssl-dev
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python-certbot-apache
